7b73d2e1edaee9392cbb6497eaff476116c46358
samsonkimani <samsonkimani43@gmail.com>
2023-02-27 19:43:08 +0300

    creating binary trees

--- binary_trees.h
+++ binary_trees.h
@@ -1,6 +1,9 @@
 #ifndef BINARY_TREES_H
 #define BINARY_TREES_H
 
+#include <stdio.h>
+#include <stdlib.h>
+
 /**
  * struct binary_tree_s - Binary tree node
  *
@@ -27,5 +30,9 @@ typedef struct binary_tree_s avl_t;
 
 /* max binary heap */
 typedef struct binary_tree_s heap_t;
+
+/* functions */
+void binary_tree_print(const binary_tree_t *);
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 
 #endif

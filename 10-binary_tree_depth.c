3890ff93f9c42dfab4abc56fab2d42df87d6bcce
samsonkimani <samsonkimani43@gmail.com>
2023-02-28 19:01:19 +0300

    determining the depth of a node

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -9,16 +9,13 @@
 
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
-	size_t height_right = 1;
-	size_t height_left = 1;
+	size_t node_depth = 0;
 
-	if (!tree)
-		return (0);
-	if (tree->left)
-		height_left = 1 + binary_tree_depth(tree->left);
-	if (tree->right)
-		height_right = 1 + binary_tree_depth(tree->left);
-	if (height_right > height_left)
-		return (height_right);
-	return (height_left);
+	while (tree->parent)
+	{
+		node_depth++;
+		tree = tree->parent;
+	}
+	return (node_depth);
+
 }

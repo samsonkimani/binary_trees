eead001747c965a3cc724eaaa1ba989c8056487b
samsonkimani <samsonkimani43@gmail.com>
2023-02-28 18:36:30 +0300

    finding depth of a node

---
+++ 10-binary_tree_depth.c
@@ -0,0 +1,24 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_depth - a function that determines the deoth of a binary tree
+ * @tree: the root node
+ *
+ * Return: the depth of the binary tree
+ */
+
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+	size_t height_right = 0;
+	size_t height_left = 0;
+
+	if (!tree)
+		return (0);
+	if (tree->left)
+		height_left = 1 + binary_tree_depth(tree->left);
+	if (tree->right)
+		height_right = 1 + binary_tree_depth(tree->left);
+	if (height_right > height_left)
+		return (height_right);
+	return (height_left);
+}

1923c5506f016a236a217fc5a17bb7443caa3cb3
samsonkimani <samsonkimani43@gmail.com>
2023-03-03 20:01:39 +0300

    finding the ancestor of a node

---
+++ 100-binary_trees_ancestor.c
@@ -0,0 +1,28 @@
+#include "binary_trees.h"
+
+/**
+ * binary_trees_ancestor - a function that returns the ancestor of anode
+ * @first: the first element
+ * @second: the second element
+ *
+ * Return: the ancestro
+ */
+
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
+		const binary_tree_t *second)
+{
+	binary_tree_t *mom, *pop;
+
+	if (!first || !second)
+		return (NULL);
+	if (first == second)
+		return ((binary_tree_t *)first);
+	mom = first->parent;
+	pop = second->parent;
+
+	if (first == pop || !mom || (!mom->parent && pop))
+		return (binary_trees_ancestor(first, pop));
+	else if (mom == second || !pop || (!pop->parent && mom))
+		return (binary_trees_ancestor(mom, second));
+	return (binary_trees_ancestor(mom, pop));
+}
